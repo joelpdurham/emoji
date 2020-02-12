dc068a71ceaa8b8b9f8a68a9acd8d568d06d359e
joelpdurham <joeldur@gmail.com>
2020-02-12 14:06:26 -0800

    can handle eating snacks

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,9 @@
 export const reducer = (state, action) => {
+  switch(action) {
+    case 'DRINK_COFFEE':
+      return { ...state, coffees: state.coffees + 1 };
+    case 'EAT_SNACK':
+      return { ...state, snacks: state.snacks + 1 };
+  }
 
-  return { ...state, coffees: state.coffees + 1 };
 };
--- src/reducers/reducer.test.js
+++ src/reducers/reducer.test.js
@@ -2,11 +2,20 @@ import { reducer } from './reducer';
 
 describe('Reducer Test', () => {
   it('can handles the coffee case', () => {
-    const action = 'DRINK COFFEE';
+    const action = 'DRINK_COFFEE';
     const initialState = { coffees: 0 };
 
     const newState = reducer(initialState, action);
 
     expect(newState).toEqual({ coffees: 1 });
+  });
+
+  it('can handles the eat snack case', () => {
+    const action = 'EAT_SNACK';
+    const initialState = { snacks: 0 };
+
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({ snacks: 1 });
   });
 });
