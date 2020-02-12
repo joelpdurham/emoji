b1cd48137978286c7c876f890c59ed3e8bd39671
joelpdurham <joeldur@gmail.com>
2020-02-12 14:10:13 -0800

    wrote failing study test

--- src/reducers/reducer.test.js
+++ src/reducers/reducer.test.js
@@ -1,7 +1,7 @@
 import { reducer } from './reducer';
 
 describe('Reducer Test', () => {
-  it('can handles the coffee case', () => {
+  it('can handle the coffee case', () => {
     const action = 'DRINK_COFFEE';
     const initialState = { coffees: 0 };
 
@@ -10,7 +10,7 @@ describe('Reducer Test', () => {
     expect(newState).toEqual({ coffees: 1 });
   });
 
-  it('can handles the eat snack case', () => {
+  it('can handle the eat snack case', () => {
     const action = 'EAT_SNACK';
     const initialState = { snacks: 0 };
 
@@ -19,12 +19,21 @@ describe('Reducer Test', () => {
     expect(newState).toEqual({ snacks: 1 });
   });
 
-  it('can handles the take nap case', () => {
+  it('can handle the take nap case', () => {
     const action = 'TAKE_NAP';
     const initialState = { naps: 0 };
 
     const newState = reducer(initialState, action);
 
     expect(newState).toEqual({ naps: 1 });
+  });
+
+  it('can handle the study case', () => {
+    const action = 'STUDY';
+    const initialState = { study: 0 };
+
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({ study: 1 });
   });
 });
