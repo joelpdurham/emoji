07ee306597d3d74f40e1a929fd404b0bd345826d
joelpdurham <joeldur@gmail.com>
2020-02-12 16:00:42 -0800

    it knows when it has pooped

--- src/selectors/faceSelector.js
+++ src/selectors/faceSelector.js
@@ -2,6 +2,7 @@ export const isTired = state => state.coffees < 1 && state.naps < 1;
 export const isHyper = state => state.coffees > 3;
 export const isEducated = state => state.studies > 2;
 export const isHungry = state => state.snacks < 1;
+export const hasPooped = state => state.coffees > 9;
 
 export const getFace = state => {
   if(isTired(state) && isHungry(state)) return '😠';
